dc0433957da6864b9d39aa942270abbce2f44640
iiSnipsii <mac8@129mac18.valleychristian.net>
2017-01-17 14:29:34 -0800

    Added negative button, and changed the size again

--- calculator.js
+++ calculator.js
@@ -7,9 +7,25 @@ function d(val) {
 	document.getElementById("d").value = val;
 }
 
+
 // Type numbers and operators
 function v(val) {
-	document.getElementById("d").value += val;
+	
+	if(val == ".") {
+		if(decimal == false) {
+			document.getElementById("d").value += val;
+			decimal = true;
+		}
+	} else if(val == '+' || val == '-' || val == '*' || val == '/') {
+		decimal = false;
+		document.getElementById("d").value += val;
+	} else {
+		document.getElementById("d").value += val;
+	}
+}
+
+function m(val) {
+	document.getElementById("d").value *= val;
 }
 
 // Evaluate the equation
